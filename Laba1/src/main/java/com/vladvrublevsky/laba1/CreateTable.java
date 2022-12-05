dbd983b696545ab26b4b08561da5339ed8cd70df
AginS <agins.main@gmail.com>
2022-12-05 17:59:45 +0300

    Update CreateTable.java

--- Laba1/src/main/java/com/vladvrublevsky/laba1/CreateTable.java
+++ Laba1/src/main/java/com/vladvrublevsky/laba1/CreateTable.java
@@ -6,22 +6,28 @@ import java.sql.Statement;
 
 public class CreateTable {
     public static void main(String[] args) {
-        // Open a connection
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
-            //creating connection with the database
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testDB","root","MyNewPass");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testDB", "root", "MyNewPass");
             Statement stmt = con.createStatement();
-            String sql = "CREATE TABLE REGISTRATION " +
+            String sql = "CREATE TABLE IF NOT EXISTS REGISTRATION " +
                     "(id INTEGER not NULL, " +
                     " login VARCHAR(255), " +
-                    " passHash VARBINARY(255), " +
+                    " pass VARCHAR(255), " + // тут бы соль+хэш но так лень...... как-нибудь потом?
+                    " isSuperUser BIT, " +
                     " PRIMARY KEY ( login ))";
 
             stmt.executeUpdate(sql);
-            System.out.println("Created table in given database...");
+            System.out.println("Table created");
+
+            stmt.executeUpdate("INSERT IGNORE INTO REGISTRATION " + "VALUES (1001, 'Simpson', '123', 1)");
+            stmt.executeUpdate("INSERT IGNORE INTO REGISTRATION " + "VALUES (1002, 'Mcbeal', '456', 0)");
+            stmt.executeUpdate("INSERT IGNORE INTO REGISTRATION " + "VALUES (1003, 'Flinstone', '789', 0)");
+            stmt.executeUpdate("INSERT IGNORE INTO REGISTRATION " + "VALUES (1004, 'Cramden', '000', 1)");
+
+            System.out.println("Values inserted");
             stmt.close();
         } catch(Exception e) {
             e.printStackTrace();
